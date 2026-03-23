5d43178c5d4c7c421c04e7b2a459c405af800b7a
Hala M. Dalloul <haladalloul42@gmail.com>
2026-03-23 09:38:14 +0200

    Implement the top bar component and update the Home Screen layout.
    - Create `BeautyTopBar` component featuring a greeting, a search icon, and a notification icon with optional badge support.
    - Refactor `HomeScreen` to use `Scaffold` and `LazyColumn` for its content structure.
    - Add `navController` parameter to `HomeScreen` and implement a UI preview.
    - Include Toast messages for search and notification icons in `BeautyTopBar`.

--- app/src/main/java/com/example/newsalon/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/newsalon/presentation/screens/home/HomeScreen.kt
@@ -1,15 +1,53 @@
 package com.example.newsalon.presentation.screens.home
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.example.newsalon.R
+import com.example.newsalon.presentation.navigation.BottomNavigationBar
+import com.example.newsalon.presentation.screens.components.BeautyTopBar
+import com.example.newsalon.presentation.screens.components.FeatureBanner
+import com.example.newsalon.presentation.screens.components.ProductCard
+import com.example.newsalon.presentation.theme.BeautyAppTheme
+import com.example.newsalon.domain.models.Product
+import com.example.newsalon.presentation.navigation.NavItem
+import com.example.newsalon.presentation.navigation.Screen
 
 @Composable
-fun HomeScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "Welcome to Home Screen!")
+fun HomeScreen(navController: NavController) {
+    Scaffold(
+
+        topBar = { BeautyTopBar(true,isShowNotification = true)},
+    ) { innerPadding->
+        LazyColumn(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+
+        }
+    }
+}
+@Preview
+@Composable
+fun PreviewFun(){
+    BeautyAppTheme {
+        HomeScreen(rememberNavController())
     }
 }
